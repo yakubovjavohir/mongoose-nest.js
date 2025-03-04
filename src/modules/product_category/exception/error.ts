7d1a49bb886db9fdeb16ef02fcef2e4d5604a4e3
javohir9999 <javohiryoqubov182@gmail.com>
2025-03-05 01:37:53 +0500

    pr

--- src/modules/product_category/exception/error.ts
+++
@@ -1,7 +0,0 @@
-import { HttpException, HttpStatus } from "@nestjs/common";
-
-export class ProductCategoryNotFound extends HttpException{
-    constructor(){
-        super("product bilan category bog'lanmagan", HttpStatus.NOT_FOUND)
-    }
-}
